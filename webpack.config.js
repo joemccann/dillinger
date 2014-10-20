d1ebacf913a4563039f4529d0a3b4df50c25e9f7
Martin Broder <mrtnbroder@gmail.com>
2014-10-21 01:18:28 +0200

    integrate angular for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 var path = require("path");
-// var webpack = require("webpack");
+var webpack = require("webpack");
+
+var nodeModulesPath = path.join(__dirname, 'node_modules');
+// var bowerComponentsPath = path.join(__dirname, 'bower_components');
+
+// var ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
 
 module.exports = {
   cache: true,
@@ -12,13 +17,16 @@ module.exports = {
   },
   module: {
     noParse: [
-    /brace/
+      /brace/,
+      /angular/
     ],
     loaders: [
       // required to write "require('./style.css')"
-      { test: /\.css$/,    loader: "style-loader!css-loader" },
-      { test: /\.handlebars$/,    loader: "handlebars-loader" },
-      { test: /\.coffee$/,    loader: "coffee-loader" }
+      { test: /\.css$/,         loader: "style-loader!css-loader" },
+      { test: /\.handlebars$/,  loader: "handlebars-loader" },
+      { test: /\.coffee$/,      loader: "coffee-loader" },
+      // export angular
+      // { test: /[\/]angular\.js$/, loader: "exports?angular" },
 
       // required for bootstrap icons
       // { test: /\.woff$/,   loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
@@ -28,7 +36,23 @@ module.exports = {
     ]
   },
   resolve: {
+    // root: [nodeModulesPath, bowerComponentsPath],
+    // alias: {
+    //   angular: 'angular/angular',
+    //   angularRouter: 'angular-ui-router/release/angular-ui-router'
+    // },
     extensions: ["", ".webpack.js", ".web.js", ".coffee", ".handlebars", ".js"]
   },
-  plugins: []
+  resolveLoader: {
+      root: nodeModulesPath
+  },
+  plugins: [
+    // new webpack.ContextReplacementPlugin(/.*$/, /a^/),
+    new webpack.ProvidePlugin({
+      'angular': 'exports?angular!angular'
+    })
+    // new webpack.ResolverPlugin([
+    //   new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    // ])
+  ]
 };
