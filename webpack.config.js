252de0143e9005f920a1228a8fff9fd73974e101
Martin Broder <mrtnbroder@gmail.com>
2014-10-29 01:38:33 +0100

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-var path = require("path");
-var webpack = require("webpack");
 
-var nodeModulesPath = path.join(__dirname, 'node_modules');
+'use strict';
+
+var
+  path            = require('path'),
+  webpack         = require('webpack'),
+  nodeModulesPath = path.join(__dirname, 'node_modules');
 
 module.exports = {
   cache: true,
-  entry: "./public/js/app.js",
+  entry: './public/js/app.js',
   output: {
-    path: path.join(__dirname, "public/js")
+    path: path.join(__dirname, 'public/js')
   },
   module: {
     noParse: [
@@ -15,24 +18,22 @@ module.exports = {
       /angular/
     ],
     loaders: [
-
-      { test: /\.css$/,         loader: "style-loader!css-loader" }
+      { test: /\.css$/,         loader: 'style-loader!css-loader' }
 
       // required for bootstrap icons
-      // { test: /\.woff$/,   loader: "url-loader?prefix=font/&limit=5000&mimetype=application/font-woff" },
-      // { test: /\.ttf$/,    loader: "file-loader?prefix=font/" },
-      // { test: /\.eot$/,    loader: "file-loader?prefix=font/" },
-      // { test: /\.svg$/,    loader: "file-loader?prefix=font/" },
+      // { test: /\.woff$/,   loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff' },
+      // { test: /\.ttf$/,    loader: 'file-loader?prefix=font/' },
+      // { test: /\.eot$/,    loader: 'file-loader?prefix=font/' },
+      // { test: /\.svg$/,    loader: 'file-loader?prefix=font/' },
     ]
   },
   resolve: {
-    extensions: ["", ".webpack.js", ".web.js", ".handlebars", ".js"]
+    extensions: ['', '.webpack.js', '.web.js', '.handlebars', '.js']
   },
   resolveLoader: {
       root: nodeModulesPath
   },
   plugins: [
-    // new webpack.ContextReplacementPlugin(/.*$/, /a^/),
     new webpack.ProvidePlugin({
       'angular': 'exports?angular!angular'
     })
