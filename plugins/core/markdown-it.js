7c0ff8971cbbbf43da50128b2aebb811f876fd61
Steinway Wu <steinwaywhw@gmail.com>
2015-05-10 17:49:43 -0400

    remove unnecessary

--- plugins/core/markdown-it.js
+++ plugins/core/markdown-it.js
@@ -16,52 +16,6 @@ var hljs = require('highlight.js')
     });
 
 /*
-  change pre>code to pre>code.hljs for proper highlight
- */
-md.renderer.rules.fence = function (tokens, idx, options, env, self) {
-  var unescapeAll     = require('markdown-it/lib/common/utils').unescapeAll;
-  var escapeHtml      = require('markdown-it/lib/common/utils').escapeHtml;
-
-  var token = tokens[idx],
-      langName = '',
-      highlighted;
-
-  if (token.info) {
-    langName = unescapeAll(token.info.trim().split(/\s+/g)[0]);
-    token.attrPush(['class', options.langPrefix + langName + ' hljs']);
-  } else {
-    token.attrPush(['class', 'hljs']);
-  }
-
-  if (options.highlight) {
-    highlighted = options.highlight(token.content, langName) || escapeHtml(token.content);
-  } else {
-    highlighted = escapeHtml(token.content);
-  }
-
-  return  '<pre' + self.renderAttrs(token) + '><code>'
-        + highlighted
-        + '</code></pre>\n';
-};
-
-/*
-  change table to table.table for proper highlight with Bootstrap
- */
-md.renderer.rules.table_open = function (tokens, idx, options, env, self) {
-
-  var token = tokens[idx];
-  
-  var index = token.attrIndex('class');
-  if (index >= 0) {
-    token.attrs[index][1] += ' table table-striped';
-  } else {
-    token.attrPush(['class', 'table table-striped']);
-  }
-
-  return self.renderToken(tokens, idx, options);
-};
-
-/*
   change to Katex for math rendering
  */
 md.use(require('markdown-it-math'), {
