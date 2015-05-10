996a79a82a41ff2abef43482df2d96c1cd96ff6b
Steinway Wu <steinwaywhw@gmail.com>
2015-05-10 17:39:08 -0400

    move markdown-it into standalone module

--- public/js/components/preview.directive.js
+++ public/js/components/preview.directive.js
@@ -1,49 +1,8 @@
 
 'use strict';
 
-var
-  // marked = require('marked'),
-  katex = require('katex'),
-  hljs = require('highlight.js'),
-  md = require('markdown-it')({
-    linkify: true,
-    typographer: true,
-    highlight: function (str, lang) {
-      if (lang && hljs.getLanguage(lang)) {
-        return hljs.highlight(lang, str).value;
-      } else {
-        return hljs.highlightAuto(str).value;
-      }
-    }
-  });
+var md = require( 'md' ).md
 
-md.use(require('markdown-it-math'), {
-  inlineRenderer: function (str) {
-    try {
-      return '<span class="math inline">' + katex.renderToString (str) + '</span>';
-    } catch (e) {
-      return '<span class="math inline">' + e + '</span>';
-    }
-  },
-  blockRenderer: function (str) {
-    try {
-      return '<span class="math block">' + katex.renderToString (str) + '</span>';
-    } catch (e) {
-      return '<span class="math block">' + e + '</span>';
-    }
-  }
-});
-
-md
-  .use(require('markdown-it-toc'))
-  .use(require('markdown-it-footnote'))
-  .use(require('markdown-it-sub'))
-  .use(require('markdown-it-sup'))
-  .use(require('markdown-it-mark'))
-  .use(require('markdown-it-deflist'))
-  .use(require('markdown-it-ins'))
-  .use(require('markdown-it-abbr'))
-  .use(require('markdown-it-checkbox'));
 
 module.exports =
   angular
