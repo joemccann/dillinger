3e6913d2b01ebc7643542b6bde815e369987f464
Pete Redmond <redmondp@gmail.com>
2016-01-24 22:39:00 +0000

    (refactor) debounce the render function
    also include a attribute on the preview directive so debounce time can
    be set

--- public/js/components/preview.directive.js
+++ public/js/components/preview.directive.js
@@ -1,23 +1,24 @@
 
 'use strict';
 
-var md = require( 'md' ).md
-
+var md = require( 'md' ).md;
 
 module.exports =
   angular
   .module('diBase.directives.preview', [])
-  .directive('preview', function($rootScope) {
+  .directive('preview', function($rootScope, debounce) {
 
   var directive = {
     link: function(scope, el, attrs) {
+
+      var delay = attrs.debounce || 200;
 
       var refreshPreview = function(val) {
         el.html(md.render($rootScope.editor.getSession().getValue()));
         return $rootScope.$emit('preview.updated');
       };
 
-      $rootScope.editor.on('change', refreshPreview);
+      $rootScope.editor.on('change', debounce(refreshPreview, delay));
 
       return refreshPreview();
     }
