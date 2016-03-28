7dc67bd3b6fd9e57b28ff347d257a5883e247193
jonbrouse <jbrouse19@gmail.com>
2016-03-28 18:21:28 +0000

    Updated logging, fixed compose file.

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.use(onedrive)
 
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'))
-    console.log('\nhttp://' + app.get('bind-address') + app.get('port') + '\n')
+    console.log('\nhttp://' + app.get('bind-address') + ':' + app.get('port') + '\n')
 })
