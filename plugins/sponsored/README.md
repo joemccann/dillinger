0b9dabc40cdb23dc22cd8cb9e744ec3c534b0bee
Joe McCann <joseph.isaac@gmail.com>
2016-08-14 11:55:33 -0700

    initial sponsored object and html commit

---
+++ plugins/sponsored/README.md
@@ -0,0 +1,7 @@
+# Sponsored Dillinger Plugin
+
+The plugin is for advertisements to help pay Dillinger's server bills.
+
+We use buysellads.com as our provider.
+
+You only need to add your buysellads account URL to the config in the `configs` directory in the root of the Dillinger project.
---
+++ plugins/sponsored/sponsored.js
@@ -0,0 +1,87 @@
+"use strict"
+
+const request = require('request')
+	, _ = require('lodash')
+	, path = require('path')
+	, fs = require('fs')
+	;
+
+// Sponsored Object
+function Sponsored(){
+
+	this.sponsored_config_file = path.resolve(__dirname, '../../configs/sponsored/', 'sponsored-config.json')
+	this.sponsored_config = {}
+	this.isConfigEnabled = false
+
+	// This fs call blocks so make sure you only instantiate the
+	// instance of the sponsored object once.
+	if( fs.existsSync(this.sponsored_config_file) ){
+	  this.sponsored_config = require(this.sponsored_config_file)
+	  this.isConfigEnabled = true
+	} 
+	else if( process.env.sponsored_app_url !== undefined ){
+
+	  this.sponsored_config = {
+	    "url": process.env.sponsored_app_url
+	  }
+
+	  this.isConfigEnabled = true
+	  console.log('sponsored config found in environment. Plugin enabled. (URL: "' + sponsored_config.app_key + '")')
+	} 
+	else{
+	  this.sponsored_config = {
+	    "url": "YOUR_URL"
+	  }
+	  console.warn('sponsored config not found at ' + sponsored_config_file + '. Plugin disabled.')
+	}
+
+}
+
+
+// The fetchAd function does exactly that
+// @param config is required
+// @param cb is required
+Sponsored.prototype.fetchAd = function fetchAd(cb){
+
+  // nah dawg you need a callback
+  if (!cb) {
+    throw Error('fetchAd requires a callback function.')
+  }
+
+  // nah dawg, for real you need a callback that's a function
+  if ( !_.isFunction(cb) ) {
+    throw Error('fetchAd requires cb parameter to be a function')
+  }
+
+  // Go get the ad JSON
+	request(this.sponsored_config.url, function adsFetchCb(err,response,body){
+		if(err){ 
+			throw new Error(err)
+		}
+		else if (response.statusCode > 399){
+			throw new Error('Error. Response Code: ' + response.statusCode)
+		}
+		else{
+			let adJSON
+			try{
+				adJSON = JSON.parse(body)
+			}catch(e){
+				throw new Error(e)
+			} // end catch
+
+			if(process.env.DEBUG) console.log(generateAdHTML(adJSON.ads[0]))
+			else cb(adJSON.ads[0]) // based on buysellads JSON
+
+		} // end else
+	}) // end request
+  
+} // end fetchAd
+
+// Helper to generate the HTML for the ad
+Sponsored.prototype.generateAdHTML = function generateAdHTML(json){
+	return '<a href="'+json.statlink+'" rel="nofollow" target="_blank">'
+	+json.description+'</a><img src="'
+	+json.pixel+'" />'
+}
+
+module.exports = Sponsored
