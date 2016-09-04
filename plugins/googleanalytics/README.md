fcde8c226c3826602aa9c05e3f313225b75e6cb1
Joe McCann <joseph.isaac@gmail.com>
2016-09-04 09:06:03 -0700

    fixed formatting

--- plugins/googleanalytics/README.md
+++ plugins/googleanalytics/README.md
@@ -1,5 +1,5 @@
 # Google Analytics Plugin
 
 1) Sign up for GA account.
-2) Get your GA UA ID.
-3) Add your GA UAID to your `configs/googleanalytics/googleanalytics-config.json`
+2) Get your GA UA ID from your GA account.
+3) Add your GA UAID to your  `configs/googleanalytics/googleanalytics-config.json`
--- plugins/googleanalytics/googleanalytics.js
+++ plugins/googleanalytics/googleanalytics.js
@@ -1,41 +1,41 @@
 "use strict"
 
 const path = require('path')
-	, fs = require('fs')
-	;
+  , fs = require('fs')
+  ;
 
 // GoogleAnalytics Object
 function GoogleAnalytics(){
 
-	this.GoogleAnalytics_config_file = path.resolve(__dirname, '../../configs/googleanalytics/', 'googleanalytics-config.json')
-	this.GoogleAnalytics_config = {}
-	this.isConfigEnabled = false
+  this.GoogleAnalytics_config_file = path.resolve(__dirname, '../../configs/googleanalytics/', 'googleanalytics-config.json')
+  this.GoogleAnalytics_config = {}
+  this.isConfigEnabled = false
 
-	// This fs call blocks so make sure you only instantiate the
-	// instance of the GoogleAnalytics object once.
-	if( fs.existsSync(this.GoogleAnalytics_config_file) ){
-	  this.GoogleAnalytics_config = require(this.GoogleAnalytics_config_file)
-	  this.isConfigEnabled = true
-	  console.log('GoogleAnalytics config found in file. Plugin enabled. (UAID: "' 
-	  	+ this.GoogleAnalytics_config.UAID + '")')
-	} 
-	else if( process.env.GoogleAnalytics_app_url !== undefined ){
+  // This fs call blocks so make sure you only instantiate the
+  // instance of the GoogleAnalytics object once.
+  if( fs.existsSync(this.GoogleAnalytics_config_file) ){
+    this.GoogleAnalytics_config = require(this.GoogleAnalytics_config_file)
+    this.isConfigEnabled = true
+    console.log('GoogleAnalytics config found in file. Plugin enabled. (UAID: "' 
+      + this.GoogleAnalytics_config.UAID + '")')
+  } 
+  else if( process.env.GoogleAnalytics_app_url !== undefined ){
 
-	  this.GoogleAnalytics_config = {
-	    "url": process.env.GoogleAnalytics_app_url
-	  }
+    this.GoogleAnalytics_config = {
+      "url": process.env.GoogleAnalytics_app_url
+    }
 
-	  this.isConfigEnabled = true
-	  console.log('GoogleAnalytics config found in environment. Plugin enabled. (UAID: "' 
-	  	+ this.GoogleAnalytics_config.UAID + '")')
-	} 
-	else{
-	  this.GoogleAnalytics_config = {
-	    "UAID": "YOUR_UAID"
-	  }
-	  console.warn('GoogleAnalytics config not found at ' 
-	  	+ this.GoogleAnalytics_config_file + '. Plugin disabled.')
-	}
+    this.isConfigEnabled = true
+    console.log('GoogleAnalytics config found in environment. Plugin enabled. (UAID: "' 
+      + this.GoogleAnalytics_config.UAID + '")')
+  } 
+  else{
+    this.GoogleAnalytics_config = {
+      "UAID": "YOUR_UAID"
+    }
+    console.warn('GoogleAnalytics config not found at ' 
+      + this.GoogleAnalytics_config_file + '. Plugin disabled.')
+  }
 
 } // end GoogleAnalytics object
 
@@ -44,24 +44,24 @@ function GoogleAnalytics(){
 GoogleAnalytics.prototype.generateGATrackingJS = function generateGATrackingJS(){
  
   return `
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-		ga('create', '${this.GoogleAnalytics_config.UAID}', 'auto');
-		ga('send', 'pageview');
+    ga('create', '${this.GoogleAnalytics_config.UAID}', 'auto');
+    ga('send', 'pageview');
   `
 }
 
 GoogleAnalytics.prototype.generateTrackLinkClicks = function generateTrackLinkClicks(){
 
-	return `var trackOutboundLink = function(url) {
-   	ga('send', 'event', 'outbound', 'click', url, {
-    	'transport': 'beacon',
-    	'hitCallback': function(){window.open(url);}
-   	});
-		}`
+  return `var trackOutboundLink = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {
+      'transport': 'beacon',
+      'hitCallback': function(){window.open(url);}
+    });
+    }`
 
 }
 
