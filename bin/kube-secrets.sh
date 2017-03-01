3873ac7cba252232aa57f75514155e463915a647
Joe McCann <joseph.isaac@gmail.com>
2017-03-01 07:22:57 -0800

    updated secrets and deploys for bitbucket secrets

--- bin/kube-secrets.sh
+++ bin/kube-secrets.sh
@@ -7,6 +7,7 @@ kubectl create secret generic googledrive-config --from-file=configs/googledrive
 kubectl create secret generic sponsored-config --from-file=configs/sponsored/sponsored-config.json --namespace=dillinger-dev
 kubectl create secret generic googleanalytics-config --from-file=configs/googleanalytics/googleanalytics-config.json --namespace=dillinger-dev
 kubectl create secret generic medium-config --from-file=configs/medium/medium-config.json --namespace=dillinger-dev
+kubectl create secret generic bitbucket-config --from-file=configs/bitbucket/bitbucket-config.json --namespace=dillinger-dev
 kubectl create secret generic dropbox-config --from-file=configs/dropbox/dropbox-config.json --namespace=dillinger-prod
 kubectl create secret generic github-config --from-file=configs/github/github-config.json --namespace=dillinger-prod
 kubectl create secret generic onedrive-config --from-file=configs/onedrive/onedrive-config.json --namespace=dillinger-prod
@@ -14,3 +15,4 @@ kubectl create secret generic googledrive-config --from-file=configs/googledrive
 kubectl create secret generic sponsored-config --from-file=configs/sponsored/sponsored-config.json --namespace=dillinger-prod
 kubectl create secret generic googleanalytics-config --from-file=configs/googleanalytics/googleanalytics-config.json --namespace=dillinger-prod
 kubectl create secret generic medium-config --from-file=configs/medium/medium-config.json --namespace=dillinger-prod
+kubectl create secret generic bitbucket-config --from-file=configs/bitbucket/bitbucket-config.json --namespace=dillinger-prod
