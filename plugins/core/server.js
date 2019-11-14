b1cd407eca41d8130860bf0beacf37cf815b57e8
Joe McCann <joemccan@microsoft.com>
2019-11-14 12:06:55 -0800

    refactors markdown-it for es7; fixed import with latest version of markdown-it

--- plugins/core/server.js
+++ plugins/core/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = module.exports = express()
 const fs = require('fs')
 const path = require('path')
-const md = require('./markdown-it.js').md
+const md = require('./markdown-it.js')
 const breakdance = require('breakdance')
 const mdToPdf = require('md-to-pdf')
 
