54030fead097dff50083b96c620476c41840b86e
joemccann <joseph.isaac@gmail.com>
2020-04-04 09:01:21 -0700

    format

--- config.js
+++ config.js
@@ -2,8 +2,9 @@
 const rc = require('rc')
 const defaultConfig = {
   title: 'Online Markdown Editor - Dillinger, the Last Markdown Editor ever.',
-  description: 'Dillinger is an online cloud based HTML5 filled Markdown Editor. ' +
-      'Sync with Dropbox, Github, Google Drive or OneDrive. Convert HTML to Markdown. 100% Open Source!',
+  description: `Dillinger is an online cloud based HTML5 filled
+  Markdown Editor. Sync with Dropbox, Github, Google Drive or OneDrive.
+  Convert HTML to Markdown. 100% Open Source!`,
   googleWebmasterMeta: 'DAyGOgtsg8rJpq9VVktKzDkQ1UhXm1FYl8SD47hPkjA',
   keywords: 'Markdown, Dillinger, Editor, ACE, Github, Open Source, Node.js',
   author: 'Joe McCann and Martin Broder'
