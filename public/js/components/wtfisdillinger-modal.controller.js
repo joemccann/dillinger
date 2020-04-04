a24106fbbeb06eb0089b3df3ea8abbbc78515b55
joemccann <joseph.isaac@gmail.com>
2020-04-04 13:06:53 -0700

    format

--- public/js/components/wtfisdillinger-modal.controller.js
+++ public/js/components/wtfisdillinger-modal.controller.js
@@ -1,17 +1,15 @@
 
-'use strict';
+'use strict'
 
 module.exports =
   angular
-  .module('diBase.controllers.about', [])
-  .controller('WTFisDillingerModalInstance', function($scope, $modalInstance) {
+    .module('diBase.controllers.about', [])
+    .controller('WTFisDillingerModalInstance', function ($scope, $modalInstance) {
+      $scope.ok = function () {
+        return $modalInstance.close()
+      }
 
-  $scope.ok = function() {
-    return $modalInstance.close();
-  };
-
-  $scope.cancel = function() {
-    return $modalInstance.dismiss('cancel');
-  };
-
-});
+      $scope.cancel = function () {
+        return $modalInstance.dismiss('cancel')
+      }
+    })
